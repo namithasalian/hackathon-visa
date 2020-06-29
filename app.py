3ccf46c71bff3afff4ae9db31a01dc10dd5eb55e
Namitha Salian <namie1110@gmail.com>
2020-06-28 18:15:31 -0700

    Configuring the app  to run

--- app.py
+++ app.py
@@ -12,46 +12,192 @@ def hello_world():
 
 @app.route('/searchMerchants', methods=["POST"])
 def search_merchants():
-
     request_body = request.json
 
     category_code_mappings = {"FAST_FOOD_RESTAURANTS": 5814}
     print(request_body)
 
     payload = {"header": {
-                "messageDateTime": "2020-06-20T18:14:39.903",
-                "requestMessageId": "Request_001",
-                "startIndex": "0"
-                    },
-                    "searchAttrList": {
-                    "merchantCategoryCode": [
-                        category_code_mappings[request_body['category']]
-                        ],
-                    "merchantCountryCode": "840",
-                    "latitude": request_body['latitude'],
-                    "longitude": request_body['longitude'],
-                    "distance": "99",
-                    "distanceUnit": "M"
-                      },
-                    "responseAttrList": [
-                    "GNLOCATOR"
-                    ],
-                    "searchOptions": {
-	                "wildCard": [
-                    "merchantName"
-                    ],
-                    "maxRecords": "5",
-                    "matchIndicators": "true",
-                    "matchScore": "true"
-                    }
-                    }
+        "messageDateTime": "2020-06-20T18:14:39.903",
+        "requestMessageId": "Request_001",
+        "startIndex": "0"
+    },
+        "searchAttrList": {
+            "merchantCategoryCode": [
+                category_code_mappings[request_body['category']]
+            ],
+            "merchantCountryCode": "840",
+            "latitude": request_body['latitude'],
+            "longitude": request_body['longitude'],
+            "distance": "99",
+            "distanceUnit": "M"
+        },
+        "responseAttrList": [
+            "GNLOCATOR"
+        ],
+        "searchOptions": {
+            "wildCard": [
+                "merchantName"
+            ],
+            "maxRecords": "5",
+            "matchIndicators": "true",
+            "matchScore": "true"
+        }
+    }
 
     url = "https://sandbox.api.visa.com/merchantlocator/v1/locator"
 
     headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Basic Sk9IUkUyQTNHTUtHWjNLN0xPMlMyMWNCWlZlNjhHenJlencyeTF2eVpXdzIzVnNfNDpXMGUyNGZ4SVBk'
-        }
+    }
+    r = requests.post(url,
+                      cert=('Certificates/cert.pem', 'Certificates/privateKey.pem'),
+                      headers=headers,
+                      auth=("5COPOJAN7B63B74JMML121Z4flu5-fyzcdQ0HNwFM5zBxlwtc", "zhrd4VO3R4Owe7"),
+                      data=json.dumps(payload))
+    # print(r.content())
+    response = r.text.encode('utf8')
+    print(response)
+    return response
+
+
+@app.route('/fundsTransfer', methods=["POST"])
+def funds_transfer():
+    request_body = request.json
+    print(request_body)
+
+
+    payload = {
+        "acquirerCountryCode": "840",
+        "acquiringBin": "408999",
+        "amount": request_body['amount'],
+        "businessApplicationId": "AA",
+        "cardAcceptor": {
+            "address": {
+                "country": "USA",
+                "county": "081",
+                "state": "CA",
+                "zipCode": "94404"
+            },
+            "idCode": "ABCD1234ABCD123",
+            "name": "Visa Inc. USA-Foster City",
+            "terminalId": "ABCD1234"
+        },
+        "cavv": "0700100038238906000013405823891061668252",
+        "foreignExchangeFeeTransaction": "11.99",
+        "localTransactionDateTime": "2020-06-28T21:27:53",
+        "retrievalReferenceNumber": "330000550000",
+        "senderCardExpiryDate": request_body['cardExpiryDate'],
+        "senderCurrencyCode": request_body['currency'],
+        "senderPrimaryAccountNumber": request_body['cardNumber'],
+        "surcharge": "11.99",
+        "systemsTraceAuditNumber": "451001",
+        "nationalReimbursementFee": "11.22",
+        "cpsAuthorizationCharacteristicsIndicator": "Y",
+        "addressVerificationData": {
+            "street": "XYZ St",
+            "postalCode": "12345"
+        },
+        "settlementServiceIndicator": "9",
+        "colombiaNationalServiceData": {
+            "countryCodeNationalService": "170",
+            "nationalReimbursementFee": "20.00",
+            "nationalNetMiscAmountType": "A",
+            "nationalNetReimbursementFeeBaseAmount": "20.00",
+            "nationalNetMiscAmount": "10.00",
+            "addValueTaxReturn": "10.00",
+            "taxAmountConsumption": "10.00",
+            "addValueTaxAmount": "10.00",
+            "costTransactionIndicator": "0",
+            "emvTransactionIndicator": "1",
+            "nationalChargebackReason": "11"
+        },
+        "riskAssessmentData": {
+            "delegatedAuthenticationIndicator": True,
+            "lowValueExemptionIndicator": True,
+            "traExemptionIndicator": True,
+            "trustedMerchantExemptionIndicator": True,
+            "scpExemptionIndicator": True
+        },
+        "visaMerchantIdentifier": request_body['merchantId']
+    }
+
+    url = "https://sandbox.api.visa.com/visadirect/fundstransfer/v1/pullfundstransactions"
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic Sk9IUkUyQTNHTUtHWjNLN0xPMlMyMWNCWlZlNjhHenJlencyeTF2eVpXdzIzVnNfNDpXMGUyNGZ4SVBk'
+    }
+    r = requests.post(url,
+                      cert=('Certificates/cert.pem', 'Certificates/privateKey.pem'),
+                      headers=headers,
+                      auth=("5COPOJAN7B63B74JMML121Z4flu5-fyzcdQ0HNwFM5zBxlwtc", "zhrd4VO3R4Owe7"),
+                      data=json.dumps(payload))
+    # print(r.content())
+    response = r.text.encode('utf8')
+    print(response)
+   # if response.con
+    # tie the transaction identifier
+
+    payload = {
+               "acquirerCountryCode": "840",
+               "acquiringBin": "408999",
+               "amount": request_body['amount'],
+               "businessApplicationId": "AA",
+               "cardAcceptor": {
+                   "address": {
+                       "country": "USA",
+                       "county": "San Mateo",
+                       "state": "CA",
+                       "zipCode": "94404"
+                   },
+                   "idCode": "CA-IDCode-77765",
+                   "name": "Visa Inc. USA-Foster City",
+                   "terminalId": "TID-9999"
+               },
+               "localTransactionDateTime": "2020-06-26T21:58:04",
+              # "merchantCategoryCode": "6012",
+               "pointOfServiceData": {
+                   "motoECIIndicator": "0",
+                   "panEntryMode": "90",
+                   "posConditionCode": "00"
+               },
+               "recipientName": "rohan",
+               "recipientPrimaryAccountNumber": "4957030420210496",
+               "retrievalReferenceNumber": "412770451018",
+               "senderAccountNumber": request_body['cardNumber'],
+           #   "senderAddress": "901 Metro Center Blvd",
+            #   "senderCity": "Foster City",
+             #  "senderCountryCode": "124",
+              # "senderName": "Mohammed Qasim",
+              # "senderReference": "",
+             #  "senderStateCode": "CA",
+               "sourceOfFundsCode": "05",
+               "systemsTraceAuditNumber": "451018",
+               "transactionCurrencyCode": request_body['currency'],
+               "transactionIdentifier": "234386193337665",
+               "settlementServiceIndicator": "9",
+               "colombiaNationalServiceData": {
+                   "countryCodeNationalService": "170",
+                   "nationalReimbursementFee": "20.00",
+                   "nationalNetMiscAmountType": "A",
+                   "nationalNetReimbursementFeeBaseAmount": "20.00",
+                   "nationalNetMiscAmount": "10.00",
+                   "addValueTaxReturn": "10.00",
+                   "taxAmountConsumption": "10.00",
+                   "addValueTaxAmount": "10.00",
+                   "costTransactionIndicator": "0",
+                   "emvTransactionIndicator": "1",
+                   "nationalChargebackReason": "11"
+               }
+               }
+    url = "https://sandbox.api.visa.com/visadirect/fundstransfer/v1/pushfundstransactions"
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic Sk9IUkUyQTNHTUtHWjNLN0xPMlMyMWNCWlZlNjhHenJlencyeTF2eVpXdzIzVnNfNDpXMGUyNGZ4SVBk'
+    }
     r = requests.post(url,
                       cert=('Certificates/cert.pem', 'Certificates/privateKey.pem'),
                       headers=headers,
@@ -66,3 +212,12 @@ def search_merchants():
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="80")
     #app.run()
+
+
+
+
+
+
+
+
+
